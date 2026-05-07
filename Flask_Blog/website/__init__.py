81a7641ec4c9440cfd3942b49758f550754f62c4
Ean Siao <20058@my.sanctamaria.school.nz>
2026-05-08 10:30:24 +1200

    added different routes to pages

--- Flask_Blog/website/__init__.py
+++ Flask_Blog/website/__init__.py
@@ -11,8 +11,10 @@ def create_app():
     
     # Import views from views.py
     from .views import views
+    from .auth import auth
     
     # Register blueprints
     app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
     
     return app
