7f8d56e86aeaf715a38b4d7eaeb9dae1ab4eb46d
Ean Siao <20058@my.sanctamaria.school.nz>
2026-05-27 13:21:56 +1200

    created sign up route

--- Flask_Blog/website/__init__.py
+++ Flask_Blog/website/__init__.py
@@ -3,10 +3,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 # Create app function
 # Returns app
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'i love rhamz dela torre'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     # Import views from views.py
     from .views import views
@@ -16,5 +22,18 @@ def create_app():
     # Register blueprints
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+    
+    from .models import User
+    
+    with app.app_context():
+        db.create_all()
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
     return app
--- Flask_Blog/website/auth.py
+++ Flask_Blog/website/auth.py
@@ -1,5 +1,14 @@
 # Import external libraries
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# Import database
+from . import db
+
+# Import from .models user
+from .models import User
+
 
 # Set auth blueprint
 auth = Blueprint("auth", __name__)
@@ -10,6 +19,35 @@ auth = Blueprint("auth", __name__)
 # Sign up fuction
 # Returns sign up page
 def sign_up():
+    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        email_exists = User.query.filter_by(email=email).first()
+        username_exists = User.query.filter_by(username=username).first()
+        if email_exists:
+            flash('Email is already in use.', category='error')
+        elif username_exists:
+            flash('Username is already in use.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) <8:
+            flash('Password is too short.', category='error')
+        elif len(username) < 2:
+            flash('Username is too short.', category='error')
+        elif len(email) < 4:
+            flash('Email is not valid.', category='error')
+        else:
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='scrypt:32768:8:1'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Your account has been created!', category='success')
+            return redirect(url_for('views.home'))
+        
     return render_template("sign_up.html")
 
 # Login route
