b09b37f545fe94a16338ecb7aa4a897c4d57f77b
Ean Siao <20058@my.sanctamaria.school.nz>
2026-06-15 13:22:18 +1200

    created post database model and table

--- Flask_Blog/website/models.py
+++ Flask_Blog/website/models.py
@@ -3,10 +3,19 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-
+# Database model for user
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    posts = db.relationship('Posts', backref='user', passive_deletes=True)
+
+# Database model for blog post
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
